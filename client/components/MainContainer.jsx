ee12886eecb82f5622d3f6bff2cc13cc9d059562
Talis Lazdins <lazdinst@gmail.com>
2017-11-14 20:05:32 -0800

    Added dispatches

--- client/components/MainContainer.jsx
+++ client/components/MainContainer.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Container, Divider } from 'semantic-ui-react';
-
-function MainContainer() {
-  return (
-    <Container text style={{ marginTop: '4em' }} >
-      This is the Main Container
-    </Container>
-  );
+import { getAPIData } from '../store/actions/app';
+class MainContainer extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render () {
+    return (
+      <Container text style={{ marginTop: '4em' }} >
+        This is the Main Container
+      </Container>
+    );
+  }
 }
 
-export default MainContainer;
+const mapStateToProps = state => ({tasks: state.app.tasks});
+const mapDispatchToProps = dispatch => bindActionCreators({getAPIData}, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
